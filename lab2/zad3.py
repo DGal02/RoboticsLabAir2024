04b68177b16fe5d75873d3e1082060822df87c70
DGalczyk <dominik.galczyk@gmail.com>
2024-10-29 20:41:21 +0100

    lab2

---
+++ lab2/zad3.py
@@ -0,0 +1,3 @@
+# Algorytm A (A-star)* w bibliotece PythonRobotics to metoda planowania ścieżki, która znajduje najkrótszą drogę od
+# punktu początkowego do celu. Wykorzystuje heurystykę (np. odległość euklidesową) do oszacowania kosztu dojścia do
+# celu, co pozwala szybciej znaleźć optymalną trasę, omijając przeszkody.
